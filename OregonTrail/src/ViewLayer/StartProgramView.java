234ab7c50e9cd3004ec43a02126562e7b42f0fd0
mthoming <mthoming@LIN-514720L.us.nelnet.biz>
2018-02-19 20:55:26 -0600

    Merge origin/master
    Conflicts:
    	OregonTrail/src/oregontrail/OregonTrail.java

---
+++ OregonTrail/src/ViewLayer/StartProgramView.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ViewLayer;
+
+/**
+ *
+ * @author mthoming
+ */
+public class StartProgramView {
+
+    public StartProgramView() {
+    }
+   /*
+    displayStartProgramView() {
+        boolean endOfView = false;
+        do {
+            doInputs = getInputs();
+            if ((inputs is null) || (inputs = 'Q')); //no inputs were entered OR the first input is Q
+                RETURN
+            ENDIF
+            endOfView = doAction(inputs)
+        } while (endOfView != true);
+        
+    }
+*/
+}
