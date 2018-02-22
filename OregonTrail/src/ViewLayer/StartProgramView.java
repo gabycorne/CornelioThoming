55f4f1becdd127960491d8ef38c794babc8fe293
mthoming <mthoming@LIN-514720L.us.nelnet.biz>
2018-02-21 21:28:13 -0600

    Merge origin/master
    Conflicts:
    	OregonTrail/src/oregontrail/OregonTrail.java

--- OregonTrail/src/ViewLayer/StartProgramView.java
+++ OregonTrail/src/ViewLayer/StartProgramView.java
@@ -13,17 +13,71 @@ public class StartProgramView {
 
     public StartProgramView() {
     }
-   /*
-    displayStartProgramView() {
+    
+    void displayStartProgramView() {        
         boolean endOfView = false;
         do {
-            doInputs = getInputs();
-            if ((inputs is null) || (inputs = 'Q')); //no inputs were entered OR the first input is Q
-                RETURN
-            ENDIF
-            endOfView = doAction(inputs)
+            String input = getInputs();
+            
+            input = input.toUpperCase();
+            
+           if (input == null || input.length() < 1 || input.equals("Q")) {
+               boolean isValidInput = false;
+               return;
+            }
+           
+            endOfView = doAction(input);
         } while (endOfView != true);
         
     }
-*/
+    
+    private String getInputs() {
+        //add "scanner" statement to get input from the user
+        
+        
+        System.out.println("**** getInputs() called ***");
+        
+        String input = new String();
+        input = "testInput";
+        
+        return input;
+        /*
+        getInputs(): String[] {
+
+        inputs = new String array one element long
+
+        Display a description of the view
+
+        valid = false
+        WHILE valid == false (no input value has been enterd)
+
+            Display the prompt message
+            Get the value entered from the keyboard
+            Trim off leading and trailing blanks from the value
+
+            IF length of the value < 1 then
+                Display "You must enter a non-blank value”
+                Continue (move to the top of the loop and repeat)
+            ENDIF
+
+            Assign the value to the fist position in the inputs array
+
+            valid = true (ends the loop)
+        ENDWHILE
+
+        RETURN inputs
+        */
+    }
+
+    private boolean doAction(String input) {
+        System.out.println("**** doAction() called ***");
+        System.out.println("\tinput = " + input);
+        
+        return true;
+    }    
+
+    public void display() {
+        System.out.println("*** display() called ***");
+    }
+    
 }
--- OregonTrail/src/oregontrail/OregonTrail.java
+++ OregonTrail/src/oregontrail/OregonTrail.java
@@ -5,6 +5,9 @@
  */
 
 package oregontrail;
+
+import ViewLayer.StartProgramView;
+
 /**
  *
  * @author Owner
@@ -14,56 +17,9 @@ public class OregonTrail {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-      Player playerOne = new Player();
-      InventoryItem itemOne = new InventoryItem();
-      Map mapOne = new Map();
-      Location currentLocation = new Location();
-        
-        playerOne.setName("George"); 
-        playerOne.setBestTime(7.00);
-        itemOne.setInventoryType("Wagon Wheel");
-        itemOne.setQuantityInStock(5);
-        itemOne.setRequiredAmount(50);
-        mapOne.setRowCount(2);
-        mapOne.setColumnCount(2);
-        currentLocation.setRow(1);
-        currentLocation.setColumn(5);
-        currentLocation.setVisited(true);
-        currentLocation.setAmountRemaining(200);        
-        
-        String playerOneName = playerOne.getName();
-        double playerOneTime = playerOne.getBestTime();
-        String itemOneInventoryType = itemOne.getInventoryType();
-        Actor.Father.getName();
-        Actor.Father.getDescription();
-        Actor.Mother.getName();
-        Actor.Mother.getDescription();
-        Actor.Son.getName();
-        Actor.Son.getDescription();
-        Actor.Daughter.getName();
-        Actor.Daughter.getDescription();
-        Actor.Baby.getName();
-        Actor.Baby.getDescription();
-        Actor.Grandfather.getName();
-        Actor.Grandfather.getDescription();
-        Actor.Cousin.getName();
-        Actor.Cousin.getDescription();
-        Actor.Aunt.getName();
-        Actor.Aunt.getDescription();
-        
-        System.out.println(playerOne.toString());
-        System.out.println(Actor.Father);
-        System.out.println(Actor.Mother);
-        System.out.println(Actor.Son);
-        System.out.println(Actor.Daughter);
-        System.out.println(Actor.Baby);
-        System.out.println(Actor.Grandfather);
-        System.out.println(Actor.Cousin);
-        System.out.println(Actor.Aunt);
-        System.out.println(itemOne.toString());
-        System.out.println(mapOne.toString());
-        System.out.println(currentLocation.toString());
+    public static void main(String args) {
+        ViewLayer.StartProgramView startProgramView = new StartProgramView();
+        startProgramView.display();
     }
     
 }
