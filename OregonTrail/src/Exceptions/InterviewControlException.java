64fe853234777e80f02270bbf27bd115192389c1
Owner <Owner@VAIO-PC.attlocal.net>
2018-03-22 15:03:33 -0700

    Merge origin/master
    Conflicts:
    	OregonTrail/src/ViewLayer/MainMenuView.java

---
+++ OregonTrail/src/Exceptions/InterviewControlException.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exceptions;
+
+/**
+ *
+ * @author Owner
+ */
+public class InterviewControlException extends Exception {
+
+   public InterviewControlException() {
+    }
+
+    public InterviewControlException(String string) {
+        super(string);
+    }
+
+    public InterviewControlException(String string, Throwable thrwbl) {
+        super(string, thrwbl);
+    }
+
+    public InterviewControlException(Throwable thrwbl) {
+        super(thrwbl);
+    }
+
+    public InterviewControlException(String string, Throwable thrwbl, boolean bln, boolean bln1) {
+        super(string, thrwbl, bln, bln1);
+    }
+}
